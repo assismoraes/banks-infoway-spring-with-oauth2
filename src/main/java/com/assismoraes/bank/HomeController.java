29ee5ed34ab25c991006fde6fbc3d7aa8989ef37
Assis Moraes <assis_fest@live.it>
2017-11-26 14:14:51 -0300

    Início

---
+++ src/main/java/com/assismoraes/bank/HomeController.java
@@ -0,0 +1,14 @@
+package com.assismoraes.bank;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@RequestMapping
+	public String home() {
+		return "HOME";
+	}
+	
+}
